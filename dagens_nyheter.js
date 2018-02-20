f1ef201e23be0fc120adc4a6ad06b766b7f2adf8
Peter Härder <peter@harderwork.se>
2018-02-20 14:07:26 +0100

    Remove paywall div

--- dagens_nyheter.js
+++ dagens_nyheter.js
@@ -1,2 +1,3 @@
 // Removes CSS overlay that Dagens Nyheter uses to hide content behind a front-end paywall
+$("#serviceplusPaywallpaywall-container").remove("");
 $("div").removeClass("article__body--mask");
