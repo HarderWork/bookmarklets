1fd8d0a811ff36a7ee45eecdb4ee57cc5e675f1d
Peter Härder <peter@harderwork.se>
2018-02-20 11:03:46 +0100

    Removed javascript prefix

--- dagens_nyheter.js
+++ dagens_nyheter.js
@@ -1,2 +1,2 @@
 // Removes CSS overlay that Dagens Nyheter uses to hide content behind a front-end paywall
-javascript:$("div").removeClass("article__body--mask");
+$("div").removeClass("article__body--mask");
